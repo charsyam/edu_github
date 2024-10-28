7d68be523e91c9c098d3dc1e203b0f9da7789456
charsyam <charsyam@gmail.com>
2024-10-28 12:04:19 +0900

    commit

--- tests/test_minus.py
+++ tests/test_minus.py
@@ -4,9 +4,9 @@ from src.add import minus
 
 
 def test_minus_true():
-	assert add(1, 2) == -1
-	assert add(2, 3) == -1
-	assert add(3, 3) == 0
+	assert minus(1, 2) == -1
+	assert minus(2, 3) == -1
+	assert minus(3, 3) == 0
 
 def test_minus_false():
-	assert add(4, 4) != 2
+	assert minus(4, 4) != 2
